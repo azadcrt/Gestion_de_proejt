af1339b456c3d7b4db441dc6bbdeb9988b42feb5
azadcrt <104420501+azadcrt@users.noreply.github.com>
2022-11-23 06:59:15 -0500

    fully fonctional speech reco

--- speechtest.py
+++ speechtest.py
@@ -3,12 +3,14 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 
-hellow=sr.AudioFile('hello.wav')
+hellow=sr.AudioFile('stop.wav')
 with hellow as source:
     audio = r.record(source)
 try:
     s = r.recognize_google(audio)
-    print("Text: "+s)
-except Exception as e:
-    print("Exception: "+str(e))
+    print(s)
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results")
 
