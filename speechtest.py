cb289e69c66676f3a53b1812501982afe62fb27f
azadcrt <104420501+azadcrt@users.noreply.github.com>
2022-11-23 12:26:39 +0100

    Add files via upload

--- speechtest.py
+++ speechtest.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3                                                                                
 
-import speech_recognition as sr  
+import speech_recognition as sr
+r = sr.Recognizer()
 
-# get audio from the microphone                                                                       
-r = sr.Recognizer() 
-
-print(sr.Microphone.list_microphone_names())                                                                                  
-with sr.Microphone(device_index=2) as source:                                                                       
-    print("Speak:")  
-    print(sr.Microphone(device_index=2))    
-    print(source)                                                            
-    audio = r.listen(source)   
-
+hellow=sr.AudioFile('hello.wav')
+with hellow as source:
+    audio = r.record(source)
 try:
-    print("You said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+    s = r.recognize_google(audio)
+    print("Text: "+s)
+except Exception as e:
+    print("Exception: "+str(e))
+
