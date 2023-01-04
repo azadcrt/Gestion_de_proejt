a29edbfe840ddbe6691b1775a369a6136e632f6a
RubisAuroral <azanith1610@gmail.com>
2023-01-04 10:47:45 +0100

    Interface graphique et application fusionné

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
 import sys
 import numpy as np
 import time
+import pygame
+
+pygame.init()
+
+ws = 600
+screen = pygame.display.set_mode([ws, ws])
+
+
 def ColWin(x, mat):
     for i in range(0,x):
         for j in range(0,x-2):
@@ -69,8 +77,14 @@ def Joue(taille, z, x, y, mat, m):
             x=int(x)
             y=int(y)
             if (mat[x][y]!=None and z!=None) or (x<0 or x>=taille or y<0 or y>=taille):
-                x=input()
-                y=input()
+                time.sleep(10)
+                filin = open("test", "r")
+                lignes = filin.readlines()
+                x=str(lignes[0])
+                if len(lignes)==2:
+                    x=str(lignes[1])
+                else:
+                    x=None
                 Joue(taille,z,x,y,mat,m)
             else:
                 mat[x][y]=z
@@ -81,6 +95,35 @@ def End(taille):
             if mat[i][j]==None:
                 return 0
     return -1
+
+def grill(x):
+    i=0
+    while i<x:
+        pygame.draw.line(screen, (0,0,0), ((ws/x)*(i+1),0), ((ws/x)*(i+1),600), 2)
+        pygame.draw.line(screen, (0,0,0), (0,(ws/x)*(i+1)), (600,(ws/x)*(i+1)), 2)
+        i=i+1
+
+def cross(posx,posy,x):
+    pygame.draw.line(screen, (255,0,0),(posy - (x/4) ,posx + (x/4)), (posy + (x/4) ,posx - (x/4)), int((x/4)))
+    pygame.draw.line(screen, (255,0,0),(posy + (x/4) ,posx - (x/4)), (posy - (x/4) ,posx + (x/4)), int((x/4)))
+    pygame.draw.line(screen, (255,0,0),(posy - (x/4) ,posx - (x/4)), (posy + (x/4) ,posx + (x/4)), int((x/4)))
+    pygame.draw.line(screen, (255,0,0),(posy - (x/4) ,posx - (x/4)), (posy + (x/4) ,posx + (x/4)), int((x/4)))
+        
+def circle(posx,posy,x):
+    pygame.draw.circle(screen, (0, 0, 255), (posx , posy ), (x/4))
+
+def lect(nbcol, indexx,indexy):
+    gap=ws/nbcol
+    locx=(gap*indexx)+(gap/2)
+    locy=(gap*indexy)+(gap/2)
+    loctab = [None] * 3
+    loctab[0] = locx
+    loctab[1] = locy
+    loctab[2] = gap
+    return loctab
+
+
+
 
 turn=0
 memory=0
@@ -94,6 +137,7 @@ cols = int(sys.argv[1])
 taille = rows*cols
 mat = np.array([None]*taille).reshape(rows,cols)
 while End(rows)==0:
+    screen.fill((255, 255, 255))
     filin = open("test", "r")
     lignes = filin.readlines()
     c1=str(lignes[0])
@@ -110,7 +154,25 @@ while End(rows)==0:
         memory=None
     else:
         memory=int(c1)*rows+int(c2)
+
+    loctab = lect(cols,0,0)
+    grill(int(ws/loctab[2]))
+    for i in range(0,rows):
+        for j in range(0,cols):
+            loctab = lect(cols,i,j)
+            if(mat[i][j]==0):
+                cross(loctab[0],loctab[1],loctab[2])
+            if(mat[i][j]==1):
+                circle(loctab[0],loctab[1],loctab[2])
+    
+   
+    
+
+    # Flip the display
+
+    pygame.display.flip()
     time.sleep(10)
 print('égalité')
+pygame.quit()
 quit()
 #mat = [[0,2,7,4,5],[7,7,7,8,9],[7,11,7,12,13],[0,0,52,0,0], [0,0,52,0,0]]
