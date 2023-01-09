244a687dbfcc17e3882bb212e61776137bdc8c9f
RubisAuroral <azanith1610@gmail.com>
2023-01-09 11:11:10 +0100

    Ajout nombre de coup par tout + sécurité sur les entrées

--- game.py
+++ game.py
@@ -3,12 +3,6 @@ import numpy as np
 import time
 import pygame
 
-pygame.init()
-
-ws = 600
-screen = pygame.display.set_mode([ws, ws])
-
-
 def ColWin(x, mat):
     for i in range(0,x):
         for j in range(0,x-2):
@@ -66,28 +60,41 @@ def checkWin(x,mat):
 
 
 def Joue(taille, z, x, y, mat, m):
-    print(x)
-    print(y)
     if x=="STOP" or y=="STOP":
         quit()
     else:
         if x=='UNDO' or y=='UNDO':
             Joue(taille, None, m//taille, m%taille, mat, None)
         else:
-            x=int(x)
-            y=int(y)
-            if (mat[x][y]!=None and z!=None) or (x<0 or x>=taille or y<0 or y>=taille):
+            if(len(x)==2 and len(y)==2):
+                x=int(x)
+                y=int(y)
+                if (mat[x][y]!=None and z!=None) or (x<0 or x>=taille or y<0 or y>=taille):
+                    del x
+                    del y
+                    time.sleep(10)
+                    filin = open("test", "r")
+                    lignes = filin.readlines()
+                    x=str(lignes[0])
+                    if len(lignes)==2:
+                        y=str(lignes[1])
+                    else:
+                        y=None
+                    Joue(taille,z,x,y,mat,m)
+                else:
+                    mat[x][y]=z
+            else:
+                del x
+                del y
                 time.sleep(10)
                 filin = open("test", "r")
                 lignes = filin.readlines()
                 x=str(lignes[0])
                 if len(lignes)==2:
-                    x=str(lignes[1])
+                    y=str(lignes[1])
                 else:
-                    x=None
+                    y=None
                 Joue(taille,z,x,y,mat,m)
-            else:
-                mat[x][y]=z
 
 def End(taille):
     for i in range(0, taille):
@@ -110,7 +117,7 @@ def cross(posx,posy,x):
     pygame.draw.line(screen, (255,0,0),(posy - (x/4) ,posx - (x/4)), (posy + (x/4) ,posx + (x/4)), int((x/4)))
         
 def circle(posx,posy,x):
-    pygame.draw.circle(screen, (0, 0, 255), (posx , posy ), (x/4))
+    pygame.draw.circle(screen, (0, 0, 255), (posy , posx ), (x/4))
 
 def lect(nbcol, indexx,indexy):
     gap=ws/nbcol
@@ -125,31 +132,57 @@ def lect(nbcol, indexx,indexy):
 
 
 
-turn=0
-memory=0
-if(len(sys.argv)<2):
+if(len(sys.argv)<3):
     print('Mauvais usage du jeu : python3 nomfichier taillePlateau coupParTour')
     quit()
-for i in range(1, len(sys.argv)):
-    print('argument:', i, 'value:', sys.argv[i])
+if(int(sys.argv[1])<3):
+    print('Mauvais usage du jeu : premier argument supérieur ou égal à 3')
+    quit()
+if(int(sys.argv[2])>2 or int(sys.argv[2])<0):
+    print('Mauvais usage du jeu : deuxième argument supérieur égal à 1 ou 2')
+    quit()
+
+pygame.init()
+ws = 600
+screen = pygame.display.set_mode([ws, ws])
+
+turn=0
+memory=0
+nbplay=1
 rows = int(sys.argv[1])
 cols = int(sys.argv[1])
 taille = rows*cols
 mat = np.array([None]*taille).reshape(rows,cols)
 while End(rows)==0:
     screen.fill((255, 255, 255))
+    
     filin = open("test", "r")
     lignes = filin.readlines()
     c1=str(lignes[0])
-    
     if len(lignes)==2:
         c2=str(lignes[1])
     else:
         c2=None
+
     Joue(rows, turn, c1, c2, mat, memory)
+    
+    filin = open("test", "r")
+    lignes = filin.readlines()
+    c1=str(lignes[0])
+    if len(lignes)==2:
+        c2=str(lignes[1])
+    else:
+        c2=None
+
     print(mat)
     checkWin(rows,mat)
-    turn=(turn+1)%2
+    
+    if(nbplay<int(sys.argv[2])):
+        nbplay=nbplay+1
+    else:
+        turn=(turn+1)%2
+        nbplay=1
+        
     if c1=='UNDO' or c2=='UNDO':
         memory=None
     else:
@@ -171,7 +204,7 @@ while End(rows)==0:
     # Flip the display
 
     pygame.display.flip()
-    time.sleep(10)
+    time.sleep(10)  
 print('égalité')
 pygame.quit()
 quit()
